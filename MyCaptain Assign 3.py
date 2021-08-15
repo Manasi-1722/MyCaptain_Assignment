062b5143f92eb11209520158364a947f7efcde1d
Manasi-1722 <88338443+Manasi-1722@users.noreply.github.com>
2021-08-15 16:30:53 +0530

    Add files via upload

---
+++ MyCaptain Assign 3.py
@@ -0,0 +1,17 @@
+# Task 2
+
+list1 = [12,-7,5,64,-14]
+for i in list1:
+    if i > 0:
+        print(i)
+
+
+list2 = [12,14,-95,3]
+for i in list2:
+    if i<=0:
+        list2.remove(i)
+print(list2)
+
+
+
+
